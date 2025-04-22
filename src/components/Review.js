747d7a06c84a63e6254515c3f4e9f97ef39bba86
Aryan Raj <aryanraj24032002@gmail.com>
2025-04-22 22:32:06 +0530

    first commit

---
+++ src/components/Review.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./../styles/Review.css";
+
+const Review = ({ data }) => {
+  const [currentId, setCurrentId] = useState(0);
+
+  function handlePrev() {
+    setCurrentId((value) => (value === 0 ? data.length - 1 : value - 1));
+  }
+
+  function handleNext() {
+    setCurrentId((value) => (value === data.length - 1 ? 0 : value + 1));
+  }
+
+  function handleRandom() {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * data.length);
+    } while (randomIndex === currentId);
+    setCurrentId(randomIndex);
+  }
+
+  const current = data[currentId];
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="review">
+        <h2 key = {`author-${current.id}`} className="author">{current.name}</h2>
+        <img src={current.image} className="person-img" />
+        <h3 className="job">{current.job}</h3>
+        <p className="info">{current.text}</p>
+        <div className="buttonContainer">
+          <button className="prev-btn" onClick={handlePrev}>
+            Previous
+          </button>
+          <button className="random-btn" onClick={handleRandom}>
+            Suprise me
+          </button>
+          <button className="next-btn" onClick={handleNext}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+
+
+};
+
+export default Review;
+
+
